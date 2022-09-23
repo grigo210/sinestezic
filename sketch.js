39ada810e7f455441f08daffeb1000432d6e1a88
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-24 01:24:46 +0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,50 @@
+let size = 300;
+let soundLaugh,soundSing;
+
+function setup() {
+  //soundFormats('mp3');
+  //soundLaugh= loadSound('path/laugh.mp3');
+ // soundSing = loadSound('https://freesound.org/people/Breviceps/sounds/457043/');
+  createCanvas(1866,925);
+  angleMode(DEGREES);
+  ellipseMode(CENTER);
+
+}
+function draw() {
+  background(0);
+  mouse();
+  for (let x = size; x <= width - size; x += 50) {
+    for (let y = size; y <= height - size; y += 50) {
+      push();
+      translate(x, y);
+      if (
+        mouseX >= size - 20 &&
+        mouseX <= width - size + 20 &&
+        mouseY >= size - 20 &&
+        mouseY <= height - size + 20
+      ) {
+      //soundLaugh.play();
+      //soundLaugh.setLoop(True);
+        rotate(atan2(mouseY - y, mouseX - x) - 90);
+      } else{
+      //soundSing.play();
+      //soundSing.setLoop(True);
+      rotate(90);     
+      }
+      fill(255);
+      ellipse(0, 0, 20, 24);
+      pop();
+    }
+  }
+}
+function mouse() {
+  if (
+    mouseX >= size - 20 &&
+    mouseX <= width - size + 20 &&
+    mouseY >= size - 20 &&
+    mouseY <= height - size + 20
+  ) {
+    fill(220, 20, 60);
+    ellipse(mouseX, mouseY, 55, 55);
+  }
+}
