843a7cf13429c1d215e4fd82e3c781aace8564b1
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 17:57:33 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,5 @@
 let size = 200;
-let t,a;
-/*
-function preload(){
- a = loadSound("laugh.mp3');
-}*/
+let script;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -30,15 +26,15 @@ function draw() {
     mouseY >= size - 20 &&
     mouseY <= height - size - 20
   ) {
-    t = "Now anyone can become a victim of their ridiculousness. \n\n Don't laugh about someone's situation...\n Cause you don't know when you might be in their place.";
+    script = "Now anyone can become a victim of their ridiculousness. \n\n Don't laugh about someone's situation...\n Cause you don't know when you might be in their place.";
    // a.loop();
   } else {
-    t = "You're the one who can decide where this world's awareness goes.\n\n Move your pointer to the people. ";
+    script = "You're the one who can decide where this world's awareness goes.\n\n Move your pointer to the people. ";
    // a.stop();
   }
   fill(255);
   textSize(16);
-  text(t, size, size, 500, 500);
+  text(script, size, size, 500, 500);
   //grid of ellipses
   for (let x = size * 5; x <= width - size * 1.5; x += 50) {
     for (let y = size; y <= height - size; y += 50) {
