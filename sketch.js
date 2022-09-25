a3683a26d18e09e4a41588fe08e1071ac16abcbd
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 16:17:40 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function draw() {
   fill(220, 20, 60);
   ellipse(mouseX, mouseY, 55, 55);
   fill(255);
-  textSize(16);
+  textSize(18);
   text(t,size,size,500,500);
   for (let x = size*5; x <= width - size*1.5; x += 50) {
     for (let y = size; y <= height - size; y += 50) {
