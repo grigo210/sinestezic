6330d7187b28eb1ebf0a0497dad99ba8b2267e11
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 15:39:52 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,9 +27,9 @@ function draw() {
       translate(x, y);
       if (
         mouseX >= size*5 - 20 &&
-        mouseX <= width - size*1.5 + 20 &&
+        mouseX <= width - size + 10 &&
         mouseY >= size - 20 &&
-        mouseY <= height - size + 15
+        mouseY <= height - size + 10
       ) {
         t = "Now everyone becomes a victim of their own sin. \n\n Don't laugh about someone's situation, because you don't know when you might be in their situation.";
         rotate(atan2(mouseY - y, mouseX - x) - 90);
@@ -47,9 +47,9 @@ function draw() {
 function mouse() {
   if (
     mouseX >= size*5 - 20 &&
-    mouseX <= width - size*1.5 + 20 &&
+    mouseX <= width - size + 10 &&
     mouseY >= size - 20 &&
-    mouseY <= height - size + 15
+    mouseY <= height - size + 10
   ) {
     fill(220, 20, 60);
     ellipse(mouseX, mouseY, 55, 55);
