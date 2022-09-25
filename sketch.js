2f429422e1aa2c2c976c4e57988f86af0b7770da
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 20:32:46 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 let size = 200;
 let script;
 let sound;
+let fade;
+let fadeAmount = 1
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   ellipseMode(CENTER);
   sound = new Audio('laugh.mp3');
+  fade = 0;
 }
 
 function draw() {
@@ -38,7 +41,7 @@ function draw() {
   textSize(16);
   text(script, size, size, 500, 500);
   textSize(11);
-  text('Created by Alexandru - Iulian Grigoraș', size, height - size - 35, 500, 100);
+  text('Created by Alexandru - Iulian Grigoraș', size, height - size - 50, 500, 100);
   //grid of ellipses
   for (let x = size * 5; x <= width - size * 1.5; x += 50) {
     for (let y = size; y <= height - size; y += 50) {
