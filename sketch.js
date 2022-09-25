6f8c93745e9be4c7afb5ebfbc4d73b78477cfffb
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 13:46:04 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function draw() {
 function mouse() {
   if (
     mouseX >= size*5 - 20 &&
-    mouseX <= width - size*1.5 + 20 &&
+    mouseX <= width - size*1.8 + 20 &&
     mouseY >= size - 20 &&
     mouseY <= height - size + 20
   ) {
