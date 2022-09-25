1dd483f3c1be9daf673559999d2d42df0cd8ab12
Alexandru - Iulian Grigoraș <59127137+grigo210@users.noreply.github.com>
2022-09-25 21:51:42 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,11 +34,11 @@ function draw() {
     mouseY <= height - size - 22
   ) {
     script = "Now anyone can become a victim of their ridiculousness. \n\n Don't laugh about someone's situation...\n Cause you don't know when you might be in their place.";
-    fadeMotto = 0;
+    fadeMotto += 1;
     sound.play();
   } else {
     script = "You're the one who can decide where this world's awareness goes.\n\n Move your pointer to the people. ";
-    fadeMotto = 0;
+    fadeMotto += 1;
     sound.pause();
   }
   fill(250,fadeMotto);
@@ -59,5 +59,4 @@ function draw() {
     }
   }
     fade += 1;
-    fadeMotto += 1;
 }
